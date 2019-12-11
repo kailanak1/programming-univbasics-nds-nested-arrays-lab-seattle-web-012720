c961f15cc2e620f41b4419c33b0381d1ef84193d
Kailana Kahawaii <kailanakahawaii@gmail.com>
2019-12-11 22:59:18 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -27,6 +27,8 @@ end
 def matrix_lookup(matrix, row, column)
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
+  produce_array = [CONVENTIONAL_PRODUCE] + [ORGANIC_PRODUCE]
+  produce_array[1][7]
 end
 
 def matrix_update(matrix, row, column, new_value)
